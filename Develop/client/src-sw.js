d9152ce816ca4323b4f68b20828d6fecbada0600
CYCBrian <143226784+CYCBrian@users.noreply.github.com>
2024-02-28 16:42:08 -0500

    2. Implemented asset caching, ADDED service worker and manifext file, ADDED CSS loaders and babel to webpack

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -27,4 +27,14 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({request}) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins:[
+      new CacheableResponsePlugin({
+        statuses:[0,200],
+      })
+    ]
+  })
+);
