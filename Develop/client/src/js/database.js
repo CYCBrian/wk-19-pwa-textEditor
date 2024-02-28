d396c5065face981d0d05191a6856e66b53a989f
CYCBrian <143226784+CYCBrian@users.noreply.github.com>
2024-02-28 17:28:08 -0500

    4. ADDED Logic to getall and post in database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,35 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) =>{
+  try{
+    console.log('Saving...')
+    const jateDb = await openDB('JATE',1);
+    const transaction = jateDb.transaction('JATE','readwrite');
+    const safe = transaction.objectStore('JATE')
+    const request = safe.add({content: content})
+    const result = await request
+    console.log('Content saved!', result)
+  } catch(error){
+    console.error("An error occurred:", error)
+    return false
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {
+  try {
+    console.log('Retrieving...');
+    const jateDb = await openDB('JATE', 1);
+    const transaction = jateDb.transaction('JATE', 'readonly');
+    const safe = transaction.objectStore('JATE');
+    const request = safe.getAll();
+    const result = await request;
+    console.log('Retrieved all content!', result);
+    return result;
+  } catch (error) {
+    console.error('An error occurred:', error);
+    return null;
+  }
+}
 initdb();
